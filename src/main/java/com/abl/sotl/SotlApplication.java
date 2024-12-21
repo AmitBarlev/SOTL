b2684d43973e5ec4316d5d3bf1a47879f1cb6da3
AmitBarlev <Barlev.amitush@gmail.com>
2024-12-21 14:22:36 +0200

    INITITLIZR

---
+++ src/main/java/com/abl/sotl/SotlApplication.java
@@ -0,0 +1,13 @@
+package com.abl.sotl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SotlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SotlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abl/sotl/SotlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abl.sotl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SotlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
