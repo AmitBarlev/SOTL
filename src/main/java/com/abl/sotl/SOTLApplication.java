2d5e5fda0d80cdab920c8b9a87bf81a1b78ef55a
ABL <barlev.amitush@gmail.com>
2024-12-21 15:07:00 +0200

    Implement Redis Connection (#2)

--- src/main/java/com/abl/sotl/SOTLApplication.java
+++ src/main/java/com/abl/sotl/SOTLApplication.java
@@ -3,8 +3,10 @@ package com.abl.sotl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication(exclude = {RedisAutoConfiguration.class})
+@EnableConfigurationProperties
 public class SOTLApplication {
 
 	public static void main(String[] args) {
